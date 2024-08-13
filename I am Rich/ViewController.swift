beb69f50f81ec12ed1a8f921eed71076f0f80f78
BoranOmerD <147264055+BoranOmerD@users.noreply.github.com>
2024-08-13 19:33:59 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Boran Ömer Doğan on 28.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
